ab215043078526d0124c1770a2b98ad87014d8dd
rdeese <rdeese@hmc.edu>
2015-09-01 17:34:09 -0700

    Working physics\!

--- js/Agent.js
+++ js/Agent.js
@@ -1,66 +1,149 @@
 // constructor
 function Agent(bounds, radius, position, velocity, genome) {
-	Shape.call(this);
+	// call inherited shape constructor
+	this.Shape_constructor();
+
 	this.snapToPixel = true;
 	this.bounds = bounds;
 	this.radius = radius;
 	this.vel = velocity;
 	this.pos = position;
+	this.acc = vec2.fromValues(0,0);
 	this.x = this.pos[0];
 	this.y = this.pos[1];
 	this.genome = genome;
+	this.age = 0;
 	this.expressPhenotype();
 	this.height = this.width = this.radius * 2;
-	this.draw();
+	this.cached = false;
+	this.wasColliding = false;
+	this.isColliding = false;
+	this.drawAgent();
+
+	// create temp vectors we'll need later
+	this.posDiff = vec2.create();
+	this.velDiff = vec2.create();
+	this.subResult = vec2.create();
 }
 
+var agentPrototype = createjs.extend(Agent, createjs.Shape);
+
 // turns genotype into phenotype
-Agent.prototype.expressPhenotype = function () {
+agentPrototype.expressPhenotype = function () {
 	// Assumes a diploid organism with one gene, and calculates the phenotypic
 	// hue as the sum of the genes from each parent.
 	this.color = chroma.hcl(this.genome[0][0] + this.genome[1][0], 100, 100);
+	console.log("hue", this.genome[0][0], this.genome[1][0]);
 }
 
-Agent.prototype.wander = function () {
-	// randomly change the acceleration
+agentPrototype.wander = function () {
 	vec2.scale(this.acc, this.acc, 0.8);
-	vec2.add(this.acc, this.acc, vec2.fromValues(random.number()-0.5,random.number()-0.5));
+	// randomly change the acceleration
+	if (random.number() < 0.05) {
+		vec2.add(this.acc, this.acc, vec2.fromValues(random.number()-0.5,random.number()-0.5));
+	}
+}
+
+agentPrototype.collide = function (other) {
+	var radii = this.radius + other.radius;
+	var collisionDepth = radii - vec2.distance(this.pos, other.pos);
+	if (collisionDepth > 0) {
+		vec2.subtract(this.velDiff, this.vel, other.vel);
+		vec2.subtract(this.posDiff, this.pos, other.pos);
+
+		// adjust position
+		vec2.normalize(this.subResult, this.posDiff);
+		vec2.scale(this.subResult, this.subResult, collisionDepth);
+		vec2.add(this.pos, this.pos, this.subResult);
+
+		// update this agent's velocity
+		var dotProd = vec2.dot(this.velDiff, this.posDiff);
+		dotProd /= vec2.sqrDist(this.pos, other.pos);
+		vec2.scale(this.subResult, this.posDiff, dotProd);
+		vec2.subtract(this.vel, this.vel, this.subResult);
+
+		vec2.negate(this.velDiff, this.velDiff);
+		vec2.negate(this.posDiff, this.posDiff);
+
+		// update other agent's velocity
+		var dotProd = vec2.dot(this.velDiff, this.posDiff);
+		dotProd /= vec2.sqrDist(this.pos, other.pos);
+		vec2.scale(this.subResult, this.posDiff, dotProd);
+		vec2.subtract(other.vel, other.vel, this.subResult);
+
+		this.isColliding = true;
+		other.isColliding = true;
+	}
 }
 	
 // draw the graphical representation of the agent
-Agent.prototype.draw = function () {
+agentPrototype.drawAgent = function () {
+	if (this.isColliding && this.collidingCacheCanvas) {
+		this.cacheCanvas = this.collidingCacheCanvas;
+		return;
+	}
+	if (!this.isColliding && this.normalCacheCanvas) {
+		this.cacheCanvas = this.normalCacheCanvas;
+		return;
+	}
+
 	var g = this.graphics;
 	g.clear();
-	// first param is stroke width
-	g.setStrokeStyle(1);
+	g.setStrokeStyle(1); // first param is stroke width
 	g.beginStroke(this.color.darken().hex());
-	g.beginFill(this.color.hex());
+	if (this.isColliding) {
+		g.beginFill(this.color.brighten().hex());
+	} else {
+		g.beginFill(this.color.hex());
+	}
 	g.drawCircle(this.radius, this.radius, this.radius);
-	this.cached = true;
+	
+	this.uncache();
+	this.cache(-1, -1, this.width+2, this.height+2);
+
+	if (this.isColliding) {
+		this.collidingCacheCanvas = this.cacheCanvas;
+	} else {
+		this.normalCacheCanvas = this.cacheCanvas;
+	}
 }
 
 // update the kinematics of the agent
-Agent.prototype.update = function () {
+agentPrototype.update = function () {
+	// get older
+	this.age += 1;
+
+	// exercise free will (acceleration)
 	this.wander();
 
+	// Iterate internal kinematics
+	vec2.scale(this.vel, this.vel, 0.99);
 	vec2.add(this.vel, this.vel, this.acc);
 	vec2.add(this.pos, this.pos, this.vel);
 	this.x = this.pos[0];
 	this.y = this.pos[1];
 	
-	// turn around if we run into the wall
-	if (this.x + this.width > this.bound.width) {
+	// elastically collide with walls
+	if (this.x + this.width > this.bounds.width) {
 		this.x = this.pos[0] = this.bounds.width - this.width - 1;
 		this.vel[0] *= -1;
 	} else if (this.x < this.bounds.x) {
 		this.x = this.pos[0] = this.bounds.x + 1;
 		this.vel[0] *= -1;
-	} else if (this.y + this.height > this.bound.height) {
+	} else if (this.y + this.height > this.bounds.height) {
 		this.y = this.pos[1] = this.bounds.height - this.height - 1;
 		this.vel[1] *= -1;
-	} else if (this.x < this.bounds.x) {
+	} else if (this.y < this.bounds.y) {
 		this.y = this.pos[1] = this.bounds.y + 1;
 		this.vel[1] *= -1;
 	}
+
+	// handle redraws and collision logic
+	if (this.wasColliding != this.isColliding) {
+		this.drawAgent();
+	}
+	this.wasColliding = this.isColliding;
+	this.isColliding = false;
 }
 
+window.Agent = createjs.promote(Agent, "Shape");
--- js/main.js
+++ js/main.js
@@ -1,50 +1,100 @@
 "use strict";
 
+var random;
+var stage;
+var bounds;
+var agents;
+var tree;
+
 function configureDefaults () {
 	// dunno if static typed arrays will play nice so let's keep
 	// it simple for now.
 	glMatrix.setMatrixArrayType(Array);
+
+	random = new PcgRandom(Date.now());
 };
 
 function handleMouseDown (event) {
 	if (!event.primary) { return; }
 	// TODO check quadtree and remove agent if click
 	// is on an agent
+	createjs.Ticker.paused = !createjs.Ticker.paused;
 }
 
 function initAgents(num) {
-	for (var i = 0; i < num; i++) {
-		var shape = new Shape();
+	agents = [];
 
+	var radius;
+	var a;
+	for (var i = 0; i < num; i++) {
+		radius = 20;
+		a = new Agent(bounds, radius,
+									vec2.fromValues(random.number() * (bounds.width-2*radius) + radius,
+																	random.number() * (bounds.height-2*radius) + radius),
+									vec2.fromValues(random.number(), random.number()),
+									[[random.number()*180], [random.number()*180]]);
+		stage.addChild(a);
+		agents.push(a);
+		tree.insert(a);
+	}
+}
+
+function updateTree() {
+	tree.clear();
+	tree.insert(agents);
+}
 
 // our update function, which gets called by Ticker and then
 // calls the stage's update in turn, which draws things
 function tick (event) {
-	// TODO iterate kinematics
-	// TODO update quadtree
-	// TODO check for collisions
+	// do nothing if we're paused
+	if (event.paused) { return; }
+
+	// check for collisions
+	var items;
+	var a;
+	var len;
+	var item;
+	for (var i = 0; i < agents.length; i++) {
+		a = agents[i];
+		items = tree.retrieve(a);
+		len = items.length;
+		for (var j = 0; j < len; j++) {
+			item = items[j];
+			if (a == item) { continue; }
+			a.collide(item);
+		}
+	}
+
+	// iterate kinematics
+	for (var i = 0; i < agents.length; i++) {
+		agents[i].update();
+	}
+
+	// update quadtree
+	updateTree();
+
+	// update the stage
 	stage.update(event);
 }
 
 function main () {
 	configureDefaults();
 	var canvas = document.querySelector("#world");
-	canvas.width = 500;
-	canvas.height = 500;
-	var stage = new Stage(canvas);
-	Ticker.setFPS(24);
+	canvas.width = window.innerWidth - 16;
+	canvas.height = window.innerHeight - 16;
+	stage = new createjs.Stage(canvas);
 	stage.addEventListener("stagemousedown", handleMouseDown);
 
 	// QuadTree setup
-	var bounds = new Rectangle(0, 0, canvas.width, canvas.height);
+	bounds = new createjs.Rectangle(0, 0, canvas.width, canvas.height);
 	// give it the bounds, false means shapes not points, and a depth of 7
-	var tree = new QuadTree(bounds, false, 7);
+	tree = new QuadTree(bounds, false, 7);
 
-	initAgents(20);
-	
-	canvas.addEventListener("mousedown", function (e) {
-		console.log(e);
-	});
+	initAgents(100);
+	//createjs.Ticker.setFPS(24);
+	createjs.Ticker.timingMode = createjs.Ticker.RAF;
+	createjs.Ticker.on("tick", tick);
 };
 
 window.onload = main;
