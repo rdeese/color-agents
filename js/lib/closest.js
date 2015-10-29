cd6888c1a2bd407b7d99f39056520192446ba906
rdeese <rdeese@hmc.edu>
2015-10-29 16:21:34 -0700

    Refinements to selection game incentive. Mostly DOM manipulation stuff.

---
+++ js/lib/closest.js
@@ -0,0 +1,18 @@
+// taken from https://raw.githubusercontent.com/jonathantneal/closest/master/closest.js
+(function (ELEMENT) {
+	ELEMENT.matches = ELEMENT.matches || ELEMENT.mozMatchesSelector || ELEMENT.msMatchesSelector || ELEMENT.oMatchesSelector || ELEMENT.webkitMatchesSelector;
+
+	ELEMENT.closest = ELEMENT.closest || function closest(selector) {
+		var element = this;
+
+		while (element) {
+			if (element.matches(selector)) {
+				break;
+			}
+
+			element = element.parentElement;
+		}
+
+		return element;
+	};
+}(Element.prototype));
